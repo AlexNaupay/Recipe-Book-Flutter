3c83756e6ad5480b636c7e540acb1c2d36095050
Alex Naupay <alexnaupay@gmail.com>
2024-12-02 22:17:19 -0500

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,22 @@ void main(){
 
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Hello World",
-      home: Text("Hello!"),
+      home: RecipeBook(),
     );
   }
+}
 
+class RecipeBook extends StatelessWidget{
+  const RecipeBook({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Title of Screen")),
+      body: const Text("Hello World content!"),
+    );
+  }
 }
