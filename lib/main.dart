b3139f2797b8a276d1a47859e8445155127d050e
Alex Naupay <alexnaupay@gmail.com>
2025-04-08 00:32:55 -0500

    feat: Add localization support
    This commit adds localization support to the app, allowing it to display text in different languages.
    
    - Added `flutter_localizations` and `intl` dependencies to `pubspec.yaml`.
    - Enabled the `generate` flag in the `flutter` section of `pubspec.yaml`.
    - Added `l10n.yaml` to configure localization settings.
    - Created `l10n/en.arb` and `l10n/es.arb` files for English and Spanish translations.
    - Updated `lib/main.dart` to include localization delegates and use localized strings.
    - Added `devtools_options.yaml`, `macos/Podfile`, `ios/Podfile`, `macos/.gitignore` and updated `README.md` to support the changes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe_book_flutter/providers/recipe_provider.dart';
 import 'package:recipe_book_flutter/screens/fav_screen.dart';
 import 'package:recipe_book_flutter/screens/home_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,12 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => RecipesProvider()),  // Singleton like
       ],
       child: MaterialApp(
+        localizationsDelegates: [
+          AppLocalizations.delegate,  // From generated code
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
         debugShowCheckedModeBanner: false,
         // debugShowMaterialGrid: true,
         title: "Hello World",
@@ -36,18 +44,18 @@ class RecipeBook extends StatelessWidget {
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: const Text(
-              "Recipe Book",
+            title: Text(
+              AppLocalizations.of(context)!.title,
               style: TextStyle(color: Colors.white),
             ),
             backgroundColor: Colors.teal,
-            bottom: const TabBar(
+            bottom: TabBar(
                 indicatorColor: Colors.teal,
                 labelColor: Colors.white,
                 unselectedLabelColor: Colors.white54,
                 tabs: [
-                  Tab(icon: Icon(Icons.home), text: 'Home'),
-                  Tab(icon: Icon(Icons.favorite), text: 'Favorites'),
+                  Tab(icon: Icon(Icons.home), text: AppLocalizations.of(context)!.homeMenu),
+                  Tab(icon: Icon(Icons.favorite), text: AppLocalizations.of(context)!.favoritesMenu),
                 ]),
           ),
           body: const TabBarView(children: [HomeScreen(), FavoriteScreen()]),
